66e5fedac3c66dfd7a73618bc14b5f703cc3e777
Pierre-André Kuntzmann <pierre-andre.kuntzmann@etud.u-picardie.fr>
2018-06-20 12:42:15 +0200

    debut de du codage des pieces

---
+++ Les echecs/Les echecs/Coordonee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_echecs
+{
+    class Coordonee
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Coordonee(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+
+    }
+}
---
+++ Les echecs/Les echecs/Pieces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les_echecs
+{
+    class Pieces
+    {
+        public string NomPiece { get; set; }
+        public char CouleurPiece { get; set; }
+        public bool Detruite { get; set; }
+        public Coordonee[] Possitions { get; set; }
+        public Coordonee Emplacement { get; set; }
+
+        public Pieces(string s,char c,int a,int b)
+        {
+            NomPiece = s;
+            CouleurPiece = c;
+            Emplacement = new Coordonee(a, b);
+        }
+
+        public void Deplacement()
+        {
+            if (NomPiece[1] == 'P')// a ajouter si il y a quelqu'un a cote de lui
+            {
+                if (Emplacement.y == 2)
+                {
+                    Possitions = new Coordonee[2];
+                    Possitions[1] = new Coordonee(Emplacement.x, Emplacement.y + 1);
+                    Possitions[2] = new Coordonee(Emplacement.x, Emplacement.y + 2);
+                }
+                else
+                {
+                    Possitions = new Coordonee[1];
+                    Possitions[1] = new Coordonee(Emplacement.x, Emplacement.y + 1);
+
+                }
+            }
+            else if (NomPiece[1] == 'T')
+            {
+                Possitions = new Coordonee[14];
+                for (int i=0;i<)
+                {
+
+                }
+            }
+            else if (NomPiece[1] == 'C')
+            {
+
+            }
+            else if (NomPiece[1] == 'F')
+            {
+
+            }
+            else if (NomPiece[1] == 'K')
+            {
+
+            }
+            else if (NomPiece[1] == 'Q')
+            {
+
+            }
+        }
+    }
+}
